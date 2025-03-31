67e601e360bab9827528ce7193daf90af5749fd8
Samuel Newman <mozzius@protonmail.com>
2025-03-31 16:09:47 +0300

    change the font back it looks bad

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,7 +33,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [`merriweather`, `inter`],
+        fonts: [`merriweather`, `open sans`],
         display: 'swap',
       },
     },
